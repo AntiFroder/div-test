183b03fd3ff66c296ebeb3d6d7e48ac47e42f92b
AntiFroder <Fomin.Ilya.Petrovich@yandex.ru>
2024-03-27 02:59:46 +0300

    init sanctum, init files to applications

---
+++ config/token_abilities.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'applications' => [
+        'application:create',
+        'application:update',
+        'application:delete',
+        'application:view',
+    ]
+];
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
 });
+Route::group(['middleware' => 'guest'], function () {
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('applications', [ApplicationController::class, 'store']);
+
+});
+
